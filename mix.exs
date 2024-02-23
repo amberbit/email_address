d9b93e2768e513efeca30facd90dca66220bf472
Hubert Łępicki <hubert.lepicki@amberbit.com>
2024-02-23 13:47:29 +0100

    Add ex_docs so we can publish to hexdocs.pm

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule EmailAddress.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 
   defp package do
